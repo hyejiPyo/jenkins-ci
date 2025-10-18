18097dabb026bea576be820c19344365bd1688a7
표혜지 <pyohyeji@MacBook-Air.local>
2025-10-18 22:58:46 +0900

    java util import

--- src/main/java/hyeji/ci/Control.java
+++ src/main/java/hyeji/ci/Control.java
@@ -2,6 +2,7 @@ package hyeji.ci;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 public class Control {
